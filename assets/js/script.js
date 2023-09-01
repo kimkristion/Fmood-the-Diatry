e53d96a92bcadd06c803a511b327c0b6b82449d0
Elvis <kriselijahk1@gmail.com>
2023-08-31 19:16:38 -0700

    added modal HTML, and JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,26 @@
+const timeNow = dayjs();
+const timeHour = timeNow.format('h');
+const timeMinutes = timeNow.format('mm');
+
+
+// add <span id="hour"></span> to include the time of day
+var timeLog = document.getElementById("hour");
+timeLog.innerHTML = timeHour + ":" + timeMinutes;
+
+
+function openModal() {
+    document.getElementById("themodal").style.display="block";
+    document.getElementById("modal").style.display="none";
+}
+
+
+function closeModal() {
+    document.getElementById("themodal").style.display="none";
+    document.getElementById("modal").style.display="block";
+   
+}
+
+
 
 // Global variables
 var foodFacts = [
